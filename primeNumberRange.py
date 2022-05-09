8d8f6e5c15f4e64753a21706077fbb75c481c2e6
Villanity <villanity@Villanitys-MacBook-Air.local>
2022-05-09 16:32:55 +0530

    Prime in Range

--- primeNumberRange.py
+++ primeNumberRange.py
@@ -4,13 +4,14 @@ def primecheck(a, b):
     count = 0
     listprime = []
     for i in range(a, b+1):
-        for j in range(a, i-1):
+        for j in range(2, i):
             if i % j == 0:
                 count += 1
         if count != 0:
             pass
         else:
             listprime.append(i)
+        count = 0
     return print(listprime)
     
             
